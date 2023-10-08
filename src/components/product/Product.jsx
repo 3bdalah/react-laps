e339bd25947d459a450cad2925c033f6351b818d
3bdalah <3bdallah.dev@gmail.com>
2023-10-08 11:22:00 +0300

    style(product): update style card product

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -7,26 +7,28 @@ export default class Product extends Component {
     let { name, price, location, quantity, id } = this.props.productInfo;
     return (
       <>
-        <div className="card   text-center ">
-          <div className="content-card bg-slate-50 w-96 m-3 rounded-md ">
+        <div className="card   text-center  ">
+          <div className="content-card bg-slate-50 w-96 m-3 rounded-md shadow p-5">
             <h2>title : {name}</h2>
             <h2>price : {price}</h2>
             <h2>location : {location}</h2>
             <h2>qountity : {quantity}</h2>
-            <button
-              className="btn btn-info bg-slate-300 rounded-sm w-56 h-7 font-mono "
-              onClick={() => this.props.trackToUpdate(id)}
-            >
-              increase qountity
-            </button>
-            <button
-              className=" bg-red-600 m-2 text-slate-200 rounded-sm w-56 h-7 font-mono "
-              onClick={() => {
-                this.props.removProduct(id);
-              }}
-            >
-              remove
-            </button>
+            <div className="group-btns flex align-items content-center items-center justify-center ">
+              <button
+                className="btn btn-info bg-slate-300 rounded-sm  w-28 h-7 font-mono shadow"
+                onClick={() => this.props.trackToUpdate(id)}
+              >
+                increase
+              </button>
+              <button
+                className=" bg-red-600 m-2 text-slate-100 rounded-sm w-28 h-7 font-mono "
+                onClick={() => {
+                  this.props.removProduct(id);
+                }}
+              >
+                remove
+              </button>
+            </div>
           </div>
         </div>
       </>
